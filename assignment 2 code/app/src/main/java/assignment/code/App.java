3228f6ae664a6e75fcfc0c0a95e84cd4f72efe33
1-e-x-x <70974831+1-e-x-x@users.noreply.github.com>
2022-03-31 13:08:49 -0400

    Added convert2D function
    Basically, csvReader and convert2D read a csv and split it into a 2D array

--- assignment 2 code/app/src/main/java/assignment/code/App.java
+++ assignment 2 code/app/src/main/java/assignment/code/App.java
@@ -9,7 +9,7 @@ public class App {
     public static ArrayList<String> csvReader(File csvFile){
        //csvReader takes a csv text file as an input and returns an array
        //row 0 of the array is the csv description.
-       //use string.split() command to split rows into items.
+       //for example, csvReader(airline_safety.csv).get(0) returns "airline,avail_seat_km_per_week,incidents_85_99,fatal_accidents_85_99,fatalities_85_99,incidents_00_14,fatal_accidents_00_14,fatalities_00_14"
        
        try{
            Scanner scanner = new Scanner(csvFile);
@@ -27,8 +27,33 @@ public class App {
        }
     }
 
+    public static String[][] convert2D(ArrayList<String> toConvert){
+        //convert2D takes the ArrayList from csvReader and turns it into a 2D array
+        //it uses the string.split() command to split rows into items.
+        //for example:
+        //  ArrayList<String> test = csvReader(airline_safety.csv)
+        //  String[][] convertTest = convert2D(test)
+        //  converTest[0][0] returns "airline"
+
+        int numColumns = toConvert.get(0).split(",").length;
+        int numRows = toConvert.size();
+        String[][] toReturn = new String[numRows][numColumns];
+        for(int rows = 0; rows < numRows; rows++){
+            String[] temp = toConvert.get(rows).split(",");
+            for(int cols = 0; cols < numColumns; cols++){
+                toReturn[rows][cols] = temp[cols];
+            }
+        }
+        return toReturn;
+    }
     public static void main(String[] args) {
         ArrayList<String> testus = csvReader(new File("C:\\Users\\Alex\\Desktop\\systems assignment 2\\airline_safety.csv"));
-        System.out.println(testus);
+        System.out.println(testus.get(0));
+        int numColumns = testus.get(0).split(",").length;
+        System.out.println(numColumns);
+
+        String[][] testus2 = convert2D(testus);
+        System.out.println(testus2[0][1]);
+
     }
 }
