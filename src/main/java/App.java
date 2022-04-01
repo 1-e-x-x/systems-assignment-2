33e6f32e6f80976c4f74fcc8a2aa1df514041f4a
MajesticShekel <44983709+MajesticShekel@users.noreply.github.com>
2022-03-31 20:28:38 -0400

    Part 2 done
    Completed part 2 of the assignment. All that's left is part 3 (a javafx bar chart) and documentation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,7 +27,7 @@ public class App {
             while (reader.hasNext()) {
                 output.add(new ArrayList<>());
                 for(String x : reader.next().split(",")) output.get(output.size()-1).add(x);
-                output.get(output.size()-1).set(output.get(output.size()-1).size()-1, output.get(output.size()-1).get(output.get(output.size()-1).size()-1)
+                if (reader.hasNext()) output.get(output.size()-1).set(output.get(output.size()-1).size()-1, output.get(output.size()-1).get(output.get(output.size()-1).size()-1)
                         .substring(0, output.get(output.size()-1).get(output.get(output.size()-1).size()-1).length()-1)); //lol fix this later
             }
             return output;
@@ -83,25 +83,88 @@ public class App {
         }
     }
 
-    public static void summarize(ArrayList<ArrayList<String>> data){
-        int min1 = -1, max1 = -1, avg1 = 0, min2 = -1, max2 = -1, avg2 = 0;
-        boolean first = true;
-        int temp;
+    //contains and updates data for each column
+    static class ColumnData {
+        public String name = "";
+        public int col, min = -1, max = -1, avg = 0;
+        public void compare(int temp) {
+            if (min == -1 || temp < min) min = temp;
+            if (max == -1 || temp > max) max = temp;
+            avg += temp;
+        }
+        public ColumnData(int t_col) { col = t_col; };
+    };
+
+    //part 2 of the assignment, summarizes data then prints to XML
+    public static void summarize(ArrayList<ArrayList<String>> data) throws ParserConfigurationException {
+        //create list of columndata objects (one for each important column
+        ColumnData[] summary = { new ColumnData(2), new ColumnData(3), new ColumnData(4), new ColumnData(5), new ColumnData(6), new ColumnData(7), new ColumnData(8) };
+        for (ColumnData x : summary) x.name = data.get(0).get(x.col);
+
+        boolean first = true; //loop through data to update column data
         for (ArrayList<String> x : data){
             if (first == true) first = false;
-            else {
-                temp = Integer.valueOf(x.get(1));
-                if (min1 == -1 || temp < min1) min1 = temp);
-                if (max1 == -1 || temp > max1) max1 = temp;
-                avg1 += temp;
-            }
+            else for (ColumnData y : summary) y.compare(Integer.valueOf(x.get(y.col)));
         }
+
+        //create document
+        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+        Element rootElement = doc.createElement("Summary");
+        doc.appendChild(rootElement);
+
+        { //add description element
+            Element stat = doc.createElement("Stat");
+            rootElement.appendChild(stat);
+
+            Element name = doc.createElement("Name");
+            name.setTextContent("column_name");
+            stat.appendChild(name);
+
+            Element min = doc.createElement("Min");
+            min.setTextContent("min_val");
+            stat.appendChild(min);
+
+            Element max = doc.createElement("Max");
+            max.setTextContent("max_val");
+            stat.appendChild(max);
+
+            Element avg = doc.createElement("Avg");
+            avg.setTextContent("avg_val");
+            stat.appendChild(avg);
+        }
+        for (ColumnData x : summary) { //fill in elements from columndata list
+            Element stat = doc.createElement("Stat");
+            rootElement.appendChild(stat);
+
+            Element name = doc.createElement("Name");
+            name.setTextContent(x.name);
+            stat.appendChild(name);
+
+            Element min = doc.createElement("Min");
+            min.setTextContent(Integer.toString(x.min));
+            stat.appendChild(min);
+
+            Element max = doc.createElement("Max");
+            max.setTextContent(Integer.toString(x.max));
+            stat.appendChild(max);
+
+            Element avg = doc.createElement("Avg");
+            avg.setTextContent(Integer.toString(x.avg/(data.size()-1)));
+            stat.appendChild(avg);
+        }
+
+        //write the file to the system
+        try (FileOutputStream output = new FileOutputStream("src\\main\\resources\\airline_summary_statistics.xml")) {
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.transform(new DOMSource(doc), new StreamResult(output));
+        } catch (IOException | TransformerException e) { e.printStackTrace(); }
     }
 
     public static void main(String[] args) throws ParserConfigurationException {
         ArrayList<ArrayList<String>> test = processData(input("src\\main\\resources\\airline_safety.csv"));
         //for (ArrayList<String> x : test) for (String y : x) System.out.println(y); //outputs entire struct line by line
-
+        summarize(test);
         output(test);
     }
 }
